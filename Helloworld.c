6a8ee7a1b2385dd43178391755209374ded19dd1
Jaydon J <jaydon9339@gmail.com>
2026-06-19 21:05:16 +0530

    Upload Helloworld.c

---
+++ Helloworld.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+
+
+int main(){
+    
+    //
+    /*printf("Hello, new wonderful World!");
+    return 0;*/
+
+
+
+    /*int d=23;
+    int r=d/2;
+    printf("Result: %d", r);
+    return 0;*/
+
+
+
+   /*char name[]="Jim";
+    printf("Hello, %s", name);
+    return 0;*/
+
+
+    
+    /*char stem = 65;
+    printf("%c", stem);
+    return 0;*/
+
+
+    /*double obj =3.2456;
+    printf("%.30lf", obj);
+    return 0;*/
+
+
+
+    /*int sub = 11/2.0;
+    printf("%d", sub);
+    return 0;*/
+
+
+
+        /*float sub = 13%2;
+    printf("%f", sub);
+    return 0;*/
+
+
+
+        /*float byt = 8==8;
+    printf("%f", byt);
+    return 0;*/
+
+
+        /*int got = (10>8) && -1;
+    printf("%d", got);
+    return 0;*/
+
+
+
+        /*float mat= !(3==4);
+    printf("%f", mat);*/
+
+
+
+    /*int pot =7;
+    pot -= 3;
+    printf("%i",pot);
+    return 0;*/
+
+
+
+    /*int tap= 5 & 7;
+    printf("%d", tap);
+    return 0;*/
+
+
+
+    /*float qee = 6>>1;
+    printf("%f", qee);
+    return 0; */
+
+
+    /*int j=4;
+    printf("%i\n",j++);
+    printf("%i\n",j++ + ++j);
+
+    printf("%i\n",j);
+
+    return 0;*/
+
+
+    /*if(6.0!=6){
+        printf("Hallo");
+    }
+    
+
+    else if(9!=9){
+
+        printf("Hola");
+    }
+
+    else if(7!=7){
+
+        printf("Hella");
+    }
+
+    else{
+
+        printf("Hills");
+    }
+return 0; */
+
+
+
+/*int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    } else {
+        printf("%d is not even.\n", num);
+    }
+    return 0;*/
+
+
+
+/*    for(int i=1; i<=23; i++){
+        printf("Jaydon J\n");
+    }*/
+
+/*int i=1;
+    while(i<6){
+printf("%d\n",i);
+    
+if(i==5){break;}
+i++;}
+printf("Done");
+return 0;*/
+int i;
+for(i=1; i<=5; i++){
+    for(int j=1; j<=5; j++){
+        printf("%d\n", i);
+    }
+    
+}return 0;
+}
